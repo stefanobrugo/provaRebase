0b84c862c2cfd0b3ff19111fff47eaabccd4b6a9
Stefano Brugo <brugo.stefano@gmail.com>
2020-12-31 20:22:42 +0100

    primo commit

---
+++ ProvaRebase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaRebase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // primo commit
+        }
+    }
+}
